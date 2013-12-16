68028748e40173d3c765790f2a58ebce78298934
James Hovious <hoviousj@users.noreply.github.com>
2013-12-16 06:51:27 +0430

    Create steganize.py
    initial commit

---
+++ steganize.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#imports
+import sys
+
+
+#   by James Hovious
+#   https://github.com/hoviousj/Kali-Linux-Toolkit-Extensions
+#
+# version: 0.1
+# release date: TODO add date
+# changelogs:
+#   * 0.1 - TODO date - Initial release
+
+#=======================================
+#DEFINE THE FUNCTIONS OF THIS SCRIPT
+#=======================================
+
+def main(args):
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+	main(sys.argv)
